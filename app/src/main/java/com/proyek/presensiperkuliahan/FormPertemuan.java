0a5676be429da17d4a3e9306952f00d898d9a344
Irvan Rizkiansyah <rizkiansyah.ir@gmail.com>
2019-11-04 00:25:33 +0700

    Tambah Test Activity Form Pertemuan 2

---
+++ app/src/main/java/com/proyek/presensiperkuliahan/FormPertemuan.java
@@ -0,0 +1,14 @@
+package com.proyek.presensiperkuliahan;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FormPertemuan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_pertemuan);
+    }
+}
